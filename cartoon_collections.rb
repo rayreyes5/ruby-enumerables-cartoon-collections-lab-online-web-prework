21f827b5e9453e92cfc03ab25106b6d19602beda
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 04:44:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves# code an argument here
 ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def summon_captain_planet# code an argument here
-summon_captain_planet(planeteer_calls) 
+summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls# code an argument here
