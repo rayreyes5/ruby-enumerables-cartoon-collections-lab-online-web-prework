bf3f73e1b917b665e2551b7bffd45ad60c8baff7
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 05:02:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,14 @@ short_words = ["puff", "go", "two"]
 long_planeteer_calls(short_words)
 #=> false
 
-
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
+#=> true
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
