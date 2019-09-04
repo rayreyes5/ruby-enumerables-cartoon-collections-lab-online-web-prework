ba0da29c84c86498d9e2b882adbd94a8c243c293
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 05:01:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,8 @@ def long_planeteer_calls# code an argument here
 short_words = ["puff", "go", "two"]
 long_planeteer_calls(short_words)
 #=> false
+
+
 end
 
 def find_the_cheese# code an argument here
